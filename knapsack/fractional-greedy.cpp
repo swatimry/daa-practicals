45e3975172e313061a19e32e8819f39e3019f867
Swati <swati2003mry@gmail.com>
2023-09-13 12:24:52 +0530

    3

--- knapsack/fractional-greedy.cpp
+++ knapsack/fractional-greedy.cpp
@@ -2,55 +2,41 @@
 using namespace std;
 struct Item {
 	int profit, weight;
-	Item(int profit, int weight)
-	{
-		this->profit = profit;
-		this->weight = weight;
-	}
-};
-
+/*	Item(int profit, int weight)
+	{   this->profit = profit;
+		this->weight = weight;}*/};
 static bool cmp(struct Item a, struct Item b)
-{
-	double r1 = (double)a.profit / (double)a.weight;
+{   double r1 = (double)a.profit / (double)a.weight;
 	double r2 = (double)b.profit / (double)b.weight;
-	return r1 > r2;
-}
-
-
+	return r1 > r2;}
 double fractionalKnapsack(int W, struct Item arr[], int N)
-{
-
-	sort(arr, arr + N, cmp);
-
-	double finalvalue = 0.0;
-
+{   sort(arr, arr + N, cmp);
+    double finalvalue = 0.0;
 	for (int i = 0; i < N; i++) {
-		
 		if (arr[i].weight <= W) {
 			W -= arr[i].weight;
 			finalvalue += arr[i].profit;
 		}
-
-	
-		else {
-			finalvalue
-				+= arr[i].profit
-				* ((double)W / (double)arr[i].weight);
-			break;
-		}
-	}
-
-
-	return finalvalue;
-}
-
-
+    	else {
+			finalvalue+= arr[i].profit* ((double)W / (double)arr[i].weight);
+			break;	}	}
+return finalvalue;}
 int main()
-{
-	int W = 50;
-	Item arr[] = { { 60, 10 }, { 100, 20 }, { 120, 30 } };
-	int N = sizeof(arr) / sizeof(arr[0]);
+{   int n;
+    cout<<"Enter no of items"<<endl;
+    cin>>n;
+    int W;
+    cout<<"Enter capacity"<<endl;
+    cin>>W;
+    Item arr[n];
 
-	cout << fractionalKnapsack(W, arr, N);
+  for(int i=0;i<n;i++){
+        cout<<"Enter weight of items"<<i+1<<endl;
+        cin>>arr[i].weight;
+        cout<<"Enter profit of items"<<i+1<<endl;
+        cin>>arr[i].profit;
+       
+        }
+    cout << fractionalKnapsack(W, arr, n);
 	return 0;
 }
